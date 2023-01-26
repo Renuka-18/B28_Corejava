b79dd96d50bb74be2d333bc1689cb69f019319e8
G RENUKA DEVI <G RENUKA DEVI@Renuka-pc>
2023-01-26 21:56:40 +0530

    first commit

--- B28/src/com/tns/inherutance/JoinedTable.java
+++ B28/src/com/tns/inherutance/JoinedTable.java
@@ -1,5 +1,34 @@
 package com.tns.inherutance;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class JoinedTable {
 
+	public static void main(String[]args) {
+		EntityManagerFactory factory=Persistence.creatEntityManagerFactory("JPA-PU");
+		EntityManager em=factory.createEntityManager();
+		em.getTranscation().begin();
+		
+		//create one employee
+		Employee employee=new Employee();
+		employee.setName("Divya");
+		employee.setSalary(50000);
+		em.persist(employee);
+		
+		//create one manager
+		Manager manager=new Manager();
+		manager.setName("Priya");
+		manager.setSalary(80000);
+		manager.setDepartmentName("Sales");
+		em.persist(manager);
+		
+		em.getTransaction().commit();
+		System.out.println("Added oneemployee and manager to database");
+		em.close();
+		factory.close();
+		
+		
+	}
 }
