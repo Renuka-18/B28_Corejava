fe47114415ef5a0bf74aa51f1f27e8dd403c20e5
G RENUKA DEVI <G RENUKA DEVI@Renuka-pc>
2023-01-26 22:25:01 +0530

    first commit

---
+++ B28/src/com/tns/hybernate/StudentService.java
@@ -0,0 +1,10 @@
+package com.tns.hybernate;
+
+import com.tns.entities.Student;
+
+public interface StudentService {
+	public abstract void addStudent(Student student);
+	public abstract void updateStudent(Student student);
+	public abstract void removeStudent(Student student);
+	public abstract Student findStudentById(int id);
+}
